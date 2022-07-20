5b232784c6e006232257ba121cf8add75316f98f
Eric Fontana <eric.fontana@indico.io>
2022-07-20 08:21:16 -0400

    Add yamls for each additional application

--- ipa.tf
+++ ipa.tf
@@ -428,6 +428,69 @@ resource "argocd_application" "ipa" {
   }
 }
 
+
+# Create Argo Application YAML for each user supplied application
+
+
+resource "github_repository_file" "custom-application-yaml" {
+
+  for_each = var.applications
+
+  repository          = data.github_repository.argo-github-repo.name
+  branch              = var.argo_branch
+  file                = "${var.argo_path}/${each.value.name}_application.yaml"
+  commit_message      = var.message
+  overwrite_on_create = true
+
+  #TODO:
+  # this allows people to make edits to the file so we don't overwrite it.
+  #lifecycle {
+  #  ignore_changes = [
+  #    content
+  #  ]
+  #}
+
+  content = <<EOT
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: ${each.value.name}
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+  labels:
+    app: ${each.value.name}
+    region: ${var.region}
+    account: ${var.aws_account}
+    name: ${var.label}
+  annotations:
+    avp.kubernetes.io/path: tools/argo/data/ipa-deploy
+spec:
+  destination:
+    server: ${module.cluster.kubernetes_host}
+    namespace: ${each.value.namespace}
+  project: ${module.argo-registration.argo_project_name}
+  syncPolicy:
+    automated:
+      prune: true
+    syncOptions:
+      - CreateNamespace=true
+  source:
+    chart: ${each.value.chart}
+    repoURL: ${each.value.repo}
+    targetRevision: ${each.value.version}
+    plugin:
+      name: argocd-vault-plugin-helm-values-expand-no-build
+      env:
+        - name: RELEASE_NAME
+          value: ${each.value.name}
+
+        - name: HELM_VALUES         
+          value: |
+            ${base64decode(each.value.values)}    
+EOT
+}
+
+# Argo App for each App
 resource "argocd_application" "argo-application" {
   depends_on = [
     module.argo-registration,
