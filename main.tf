a608d8e91a5151314cb51451a329ce82ea1b5da8
NathanOkolita <Nathan.Okolita@gmail.com>
2022-07-28 08:52:01 -0400

    [DOP-1093] Update indico-argo-registration version

--- main.tf
+++ main.tf
@@ -295,7 +295,7 @@ module "argo-registration" {
     argocd     = argocd
   }
   source                       = "app.terraform.io/indico/indico-argo-registration/mod"
-  version                      = "1.0.41"
+  version                      = "1.0.42"
   label                        = var.label
   region                       = var.region
   argo_password                = var.argo_password
