1c71fc644f573e6e38342a542ddf7b8b46c82021
Eric Fontana <eric.fontana@indico.io>
2022-08-18 04:41:01 -0400

    1.0.45

--- main.tf
+++ main.tf
@@ -297,7 +297,7 @@ module "argo-registration" {
     argocd     = argocd
   }
   source                       = "app.terraform.io/indico/indico-argo-registration/mod"
-  version                      = "1.0.44"
+  version                      = "1.0.45"
   label                        = var.label
   region                       = var.region
   argo_password                = var.argo_password
