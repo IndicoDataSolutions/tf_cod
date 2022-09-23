e46d897ae4cb2c438f8530983b8a084d41cb87a9
ashmuck <andrey.chmykh@indico.io>
2022-09-23 00:52:11 -0400

    Removed nonexistent outputs

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -4,21 +4,6 @@ output "cluster_manager_ip" {
 
 output "storage_account_key" {
   sensitive = true
-  value     = module.storage.storage_account_primary_access_key
+  value     = module.file-storage.storage_account_primary_access_key
 }
 
-output "indico_database_server_address" {
-  value = module.database.indico_database_server_address
-}
-
-output "indico_database_server_fqdn" {
-  value = module.database.indico_database_fqdn
-}
-
-output "indico_metrics_database_server_address" {
-  value = module.metrics-database.indico_database_server_address
-}
-
-output "indico_metrics_database_server_fqdn" {
-  value = module.metrics-database.indico_database_fqdn
-}
