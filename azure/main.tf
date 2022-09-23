9dde95df177175861e5f20cd9071275759a9bbb5
ashmuck <andrey.chmykh@indico.io>
2022-09-23 15:38:24 -0400

    New module for blobs

--- azure/main.tf
+++ azure/main.tf
@@ -99,7 +99,7 @@ module "blob-storage" {
     azurerm_resource_group.cod-cluster
   ]
   source               = "app.terraform.io/indico/indico-azure-blob/mod"
-  version              = "0.0.10"
+  version              = "0.0.11"
   label                = var.label
   region               = var.region
   resource_group_name  = local.resource_group_name
