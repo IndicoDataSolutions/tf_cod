c3e965a6900e1637aa80213cd7607a021ab12951
ashmuck <andrey.chmykh@indico.io>
2022-09-23 01:05:07 -0400

    Updated storage module tag

--- azure/main.tf
+++ azure/main.tf
@@ -88,7 +88,7 @@ module "key_vault_key" {
 
 module "blob-storage" {
   source               = "app.terraform.io/indico/indico-azure-blob/mod"
-  version              = "0.0.6"
+  version              = "0.0.7"
   force_destroy        = true # allows terraform to destroy non-empty buckets.
   label                = var.label
   region               = var.region
