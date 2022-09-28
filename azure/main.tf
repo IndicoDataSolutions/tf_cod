12fecf115ee8252fb6f8be33bee012a0dab6a4b1
ashmuck <andrey.chmykh@indico.io>
2022-09-28 09:49:12 -0400

    Updated module version

--- azure/main.tf
+++ azure/main.tf
@@ -107,7 +107,7 @@ module "blob-storage" {
     azurerm_resource_group.cod-cluster
   ]
   source               = "app.terraform.io/indico/indico-azure-blob/mod"
-  version              = "0.0.16"
+  version              = "0.0.17"
   label                = var.label
   region               = var.region
   current_ip           = local.current_ip
