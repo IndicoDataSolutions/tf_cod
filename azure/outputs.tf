29b0046a61edb41d7f42b03b1f7c0810f89a6416
ashmuck <andrey.chmykh@indico.io>
2022-10-03 16:05:22 -0400

    Cleaned up outputs

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -2,11 +2,6 @@ output "cluster_manager_ip" {
   value = module.cluster-manager.cluster_manager_ip
 }
 
-output "storage_account_key" {
-  sensitive = true
-  value     = module.file-storage.storage_account_primary_access_key
-}
-
 output "terraform_ip" {
   value = local.current_ip
 }
