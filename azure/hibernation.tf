e8868642a226e33a59ef22ec782562deac8411d1
ashmuck <andrey.chmykh@indico.io>
2022-10-03 22:50:50 -0400

    More fixing

--- azure/hibernation.tf
+++ azure/hibernation.tf
@@ -1,7 +1,7 @@
 
 resource "aws_route53_record" "monitoring-caa" {
   count   = var.hibernation_enabled == true ? 1 : 0
-  zone_id = data.azurerm_dns_zone.id
+  zone_id = data.azurerm_dns_zone.primary.id
   name    = lower("monitoring.${local.dns_name}")
   type    = "CAA"
   ttl     = 300
