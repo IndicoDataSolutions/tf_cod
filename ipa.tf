fa047884cd6de86cae151bc1e96d4f1574d9a7d6
Eric Fontana <eric.fontana@indico.io>
2022-10-11 16:26:03 -0400

    fixed data repo name

--- ipa.tf
+++ ipa.tf
@@ -137,7 +137,7 @@ resource "helm_release" "ipa-crds" {
     enabled: true
     installCRDs: true
  EOF
- ,${data.github_repository_file.data-crds-values.content}
+    , "${data.github_repository_file.data-crds-values.content}"
   ]
 }
 
@@ -415,7 +415,7 @@ crunchy-postgres:
       options: SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS
   
   EOF
-  ,${data.github_repository_file.data-crds-values.content}
+    , "${data.github_repository_file.data-crds-values.content}"
   ]
 }
 
