f30a2b36f97f02404b28b935ea624f2ceeb1fb34
ashmuck <andrey.chmykh@indico.io>
2022-11-02 08:58:03 -0400

    Fixed

--- azure/workload_identity.tf
+++ azure/workload_identity.tf
@@ -5,8 +5,15 @@ resource "azuread_application" "workload_identity" {
   owners       = [data.azuread_client_config.current.object_id]
 }
 
+resource "azuread_service_principal" "workload_identity" {
+  application_id               = azuread_application.workload_identity.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
+}
+
 resource "azurerm_role_assignment" "storage_account_role_assignment" {
   scope                = module.storage.storage_account_id
   role_definition_name = "Owner"
-  principal_id         = resource.azuread_application.workload_identity.object_id
+  principal_id         = resource.azuread_service_principal.workload_identity.object_id
 }
+
