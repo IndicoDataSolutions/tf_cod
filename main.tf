fb0c4cff57f3527f75e99111d16cd3181eee2efd
Eric Fontana <eric.fontana@indico.io>
2022-11-15 07:15:43 -0500

    Updated aws provider version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.74.3"
+      version = "~> 4.38.0"
     }
     time = {
       source  = "hashicorp/time"
