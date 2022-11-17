c6578c4c8bde6f3219b70d3302a8aeab1b15184a
NathanOkolita <Nathan.Okolita@gmail.com>
2022-11-17 12:34:54 -0500

    [dop-1363] Fix name for aws_acm_certificate_validation resource.

--- acm.tf
+++ acm.tf
@@ -10,13 +10,13 @@ resource "aws_acm_certificate" "alb" {
 }
 
 resource "aws_route53_record" "alb" {
-  for_each = {
+  for_each = var.use_acm ? {
     for dvo in aws_acm_certificate.alb[0].domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
-  }
+  } : {}
 
   allow_overwrite = true
   name            = each.value.name
@@ -26,7 +26,7 @@ resource "aws_route53_record" "alb" {
   zone_id         = data.aws_route53_zone.primary.zone_id
 }
 
-resource "aws_acm_certificate_validation" "example" {
+resource "aws_acm_certificate_validation" "alb" {
   count    = var.use_acm == true ? 1 : 0
   certificate_arn         = aws_acm_certificate.alb[0].arn
   validation_record_fqdns = [for record in aws_route53_record.alb[0] : record.fqdn]
