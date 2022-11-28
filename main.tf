2e7924cbdc584f64d7175211189c89da469d291d
NathanOkolita <Nathan.Okolita@gmail.com>
2022-11-28 09:10:49 -0500

    [dop-1363] Update to indico-aws-eks-cluster 7.2.2

--- main.tf
+++ main.tf
@@ -208,7 +208,7 @@ module "cluster" {
   aws_account_name           = var.aws_account
   oidc_enabled               = false
   source                     = "app.terraform.io/indico/indico-aws-eks-cluster/mod"
-  version                    = "7.2.1"
+  version                    = "7.2.2"
   label                      = var.label
   additional_tags            = var.additional_tags
   region                     = var.region
