1152dc8e22716eb6e0f57effc1253d46685be270
Eric Fontana <eric.fontana@indico.io>
2022-11-29 05:57:01 -0500

    Block dashboard until after pre-reqs

--- local_modules.tf
+++ local_modules.tf
@@ -4,9 +4,9 @@
 module "k8s_dashboard" {
   depends_on = [
    module.cluster,
-   helm_release.ipa-crds
+   helm_release.ipa-pre-requisites
   ]
-  
+
   source = "./modules/common/k8s_dashboard"
 
   local_dns_name = local.dns_name
