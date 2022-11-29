dc0e7d12b985392c81027cecced7046e5d378d0d
Eric Fontana <eric.fontana@indico.io>
2022-11-29 14:25:05 -0500

    Use the kube-oidc-proxy

--- local_modules.tf
+++ local_modules.tf
@@ -2,15 +2,17 @@
 # Include local modules here
 #
 module "k8s_dashboard" {
+  #count = var.enable_k8s_dashboard == true ? 1 : 0
+
   depends_on = [
-   module.cluster,
-   helm_release.ipa-pre-requisites
+    module.cluster,
+    helm_release.ipa-pre-requisites
   ]
 
   source = "./modules/common/k8s_dashboard"
 
-  local_dns_name = local.dns_name
+  local_dns_name       = local.dns_name
   enable_k8s_dashboard = var.enable_k8s_dashboard
-  ipa_repo = var.ipa_repo
+  ipa_repo             = var.ipa_repo
 
 }
