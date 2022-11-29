b47df7fa4710259e05d254b7e2422d8958658414
Eric Fontana <eric.fontana@indico.io>
2022-11-29 14:19:14 -0500

    Use the kube-oidc-proxy

--- modules/common/k8s_dashboard/keycloak.tf
+++ modules/common/k8s_dashboard/keycloak.tf
@@ -8,8 +8,26 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.6.0"
     }
+    restapi = {
+      source  = "Mastercard/restapi"
+      version = "1.18.0"
+    }
   }
 }
+
+provider "restapi" {
+  alias                = "restapi_headers"
+  uri                  = "https://httpbin.org"
+  debug                = true
+  write_returns_object = true
+
+  headers = {
+    X-Internal-Client = "abc123"
+    Authorization     = var.SECRET_TOKEN
+  }
+}
+
+/*
 
 data "keycloak_realm" "realm" {
   realm = "GoogleAuth"
@@ -19,3 +37,4 @@ data "keycloak_openid_client" "kube-oidc-proxy" {
   realm_id  = data.keycloak_realm.realm.id
   client_id = "kube-oidc-proxy"
 }
+*/
