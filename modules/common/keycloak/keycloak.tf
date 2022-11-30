7b44debc0f6c7f9d80e7788fa750115ac0fce5db
Eric Fontana <eric.fontana@indico.io>
2022-11-30 06:28:42 -0500

    Added support for deleting callback

--- modules/common/keycloak/keycloak.tf
+++ modules/common/keycloak/keycloak.tf
@@ -21,7 +21,7 @@ data "keycloak_openid_client" "kube-oidc-proxy" {
   client_id = "kube-oidc-proxy"
 }
 
-resource "null_resource" "register-callback-test" {
+resource "null_resource" "register-callback" {
   # Ensure this runs every time
   triggers = {
     dns_name      = var.local_dns_name
@@ -30,7 +30,7 @@ resource "null_resource" "register-callback-test" {
   }
 
   provisioner "local-exec" {
-    command = "curl -XPOST -H 'Content-Type: application/json' -H \"Authorization: Bearer ${self.triggers.client_secret}\" -v https://keycloak-service.devops.indico.io/add --data '{\"url\": \"https://k8s.${self.triggers.dns_name}/oauth2/callback\"}'"
+    command = "curl -XPOST -H 'Content-Type: application/json' -H \"Authorization: Bearer ${null_resource.register-callback.triggers.client_secret}\" -v https://keycloak-service.devops.indico.io/add --data '{\"url\": \"https://k8s.${null_resource.register-callback.triggers.dns_name}/oauth2/callback\"}'"
   }
 
   provisioner "local-exec" {
