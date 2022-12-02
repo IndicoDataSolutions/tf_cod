f7e0fae2ae91dbcb6f4d23ca683a51a7b261d0bd
Eric Fontana <eric.fontana@indico.io>
2022-12-02 07:18:28 -0500

    Add smoketest values

--- ipa.tf
+++ ipa.tf
@@ -75,7 +75,7 @@ app-edge:
             - path: /
               pathType: Prefix
   EOT
-  ) : (<<EOT
+    ) : (<<EOT
 no-overrides: "true"
   EOT
   )
@@ -539,6 +539,7 @@ spec:
       
         - name: HELM_VALUES
           value: |
+            ${base64decode(var.ipa_smoketest_values)}    
             image:
               tag: ${var.ipa_smoketest_container_tag}
             cronjob:
