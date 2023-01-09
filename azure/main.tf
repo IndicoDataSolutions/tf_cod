c0a48ee790750b378da3f48e63732121524664ad
NathanOkolita <Nathan.Okolita@gmail.com>
2023-01-09 09:48:35 -0500

    [DOP-1271] use updated argo reference to create the secret.

--- azure/main.tf
+++ azure/main.tf
@@ -110,7 +110,7 @@ module "argo-registration" {
     argocd     = argocd
   }
   source                       = "app.terraform.io/indico/indico-argo-registration/mod"
-  version                      = "1.1.6"
+  version                      = "1.1.7"
   cluster_name                 = var.label
   region                       = var.region
   argo_password                = var.argo_password
