70339a9b6891acadec3d0265a78481ac96d0b345
Eric Fontana <eric.fontana@indico.io>
2023-01-17 08:56:05 -0500

    Fixed bad merge

--- main.tf
+++ main.tf
@@ -250,11 +250,7 @@ module "cluster" {
   aws_account_name           = var.aws_account
   oidc_enabled               = false
   source                     = "app.terraform.io/indico/indico-aws-eks-cluster/mod"
-<<<<<<< HEAD
   version                    = "7.4.0"
-=======
-  version                    = "7.2.2"
->>>>>>> 1.0.15.5
   label                      = var.label
   additional_tags            = var.additional_tags
   region                     = var.region
