3c73dea8422ca3168659ae0b76c791234db494bb
Eric Fontana <eric.fontana@indico.io>
2023-01-25 18:26:58 -0500

    Install AZ cli

--- azure/openshift/main.tf
+++ azure/openshift/main.tf
@@ -74,13 +74,25 @@ data "http" "workstation-external-ip" {
 data "azuread_service_principal" "redhat-openshift" {
   display_name = "Azure Red Hat OpenShift RP"
 }
+resource "null_resource" "install_azure_cli" {
+  provisioner "local-exec" {
+    command = <<EOH
 
+      echo "Installing AzureCLI"
+      apt-get update
+            # install requirements
+      apt-get install -y curl apt-transport-https lsb-release gnupg jq
+            # add Microsoft as a trusted source
+      curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+      AZ_REPO=$(lsb_release -cs)
+      echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+      apt-get update
+      apt-get install azure-cli
 
-module "shell-kube-install-az-cli" {
-  source       = "Invicton-Labs/shell-data/external"
-  command_unix = "curl -sL https://aka.ms/InstallAzureCLIDeb | bash"
+      EOH
+
+    interpreter = ["/bin/bash", "-c"]
 }
-
 
 resource "azuread_application" "openshift-application" {
   display_name = "${var.label}-${var.region}"
