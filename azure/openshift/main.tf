6206e4c0c74c73fa6247075e883b57f8b95b7ef9
Eric Fontana <eric.fontana@indico.io>
2023-01-26 03:55:36 -0500

    Install AZ cli

--- azure/openshift/main.tf
+++ azure/openshift/main.tf
@@ -76,21 +76,9 @@ data "azuread_service_principal" "redhat-openshift" {
 }
 resource "null_resource" "install_azure_cli" {
   provisioner "local-exec" {
-    command = <<EOH
-
-      echo "Installing AzureCLI"
-      apt-get update
-            # install requirements
-      apt-get install -y curl apt-transport-https lsb-release gnupg jq
-            # add Microsoft as a trusted source
-      curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
-      AZ_REPO=$(lsb_release -cs)
-      echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-      apt-get update
-      apt-get install azure-cli
-
-      EOH
-
+    command     = <<EOH
+     az version
+    EOH
     interpreter = ["/bin/bash", "-c"]
   }
 }
