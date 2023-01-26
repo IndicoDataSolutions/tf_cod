0a9eeac3a298009904b6af676ca9921095410ed7
Eric Fontana <eric.fontana@indico.io>
2023-01-26 05:54:55 -0500

    test shell module

--- azure/openshift/main.tf
+++ azure/openshift/main.tf
@@ -89,6 +89,14 @@ resource "null_resource" "install_azure_cli" {
   }
 }
 
+module "get-kube-credentials" {
+  source       = "Invicton-Labs/shell-data/external"
+  command_unix = <<EOH
+    env|sort
+    pwd
+    az aro list-credentials --name ${var.label} --resource-group ${var.resource_group_name} --output json
+  EOH
+}
 
 resource "azuread_application" "openshift-application" {
   display_name = "${var.label}-${var.region}"
