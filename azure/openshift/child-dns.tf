765a77a04991fd631b2900e485e6523bb59ca1f4
Eric Fontana <eric.fontana@indico.io>
2023-01-27 08:42:48 -0500

    Create the Child zone NS records in the parent

--- azure/openshift/child-dns.tf
+++ azure/openshift/child-dns.tf
@@ -9,7 +9,7 @@ resource "azurerm_dns_zone" "child-zone" {
 resource "azurerm_dns_ns_record" "example" {
   name                = var.label
   zone_name           = data.azurerm_dns_zone.domain.name
-  resource_group_name = azurerm_resource_group.cod-cluster.name
+  resource_group_name = data.azurerm_resource_group.domain.name
   ttl                 = 60
   records             = azurerm_dns_zone.child-zone.name_servers
 }
