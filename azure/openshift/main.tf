a9b6993da1015ca23334fded6cf765841772758c
Eric Fontana <eric.fontana@indico.io>
2023-01-27 04:32:56 -0500

    Use public domain

--- azure/openshift/main.tf
+++ azure/openshift/main.tf
@@ -257,7 +257,7 @@ module "cluster" {
   ]
   subscriptionId    = split("/", data.azurerm_subscription.primary.id)[2]
   pull_secret       = jsondecode(data.vault_kv_secret_v2.terraform-redhat.data_json)["openshift-pull-secret"]
-  cluster_domain    = local.dns_name
+  cluster_domain    = "{var.name}-{var.region}"
   source            = "./modules/openshift-cluster"
   label             = var.label
   region            = var.region
