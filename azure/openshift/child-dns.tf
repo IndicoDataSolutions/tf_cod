7be86f56ccdb823e125bd4a14c12171c72b21988
Eric Fontana <eric.fontana@indico.io>
2023-01-27 08:34:47 -0500

    Added msising file

---
+++ azure/openshift/child-dns.tf
@@ -0,0 +1,15 @@
+
+resource "azurerm_dns_zone" "child-zone" {
+  name                = local.dns_name
+  resource_group_name = azurerm_resource_group.cod-cluster.name
+}
+
+
+# create ns record for sub-zone in parent zone
+resource "azurerm_dns_ns_record" "example" {
+  name                = var.label
+  zone_name           = data.azurerm_dns_zone.domain.name
+  resource_group_name = azurerm_resource_group.cod-cluster.name
+  ttl                 = 60
+  records             = azurerm_dns_zone.child-zone.name_servers
+}
