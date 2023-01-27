e96d51f333447480c7a90fbd215165dc8de8ea7c
Eric Fontana <eric.fontana@indico.io>
2023-01-27 13:27:36 -0500

    Added dummmy module

--- azure/openshift/openshift-credentials.tf
+++ azure/openshift/openshift-credentials.tf
@@ -13,6 +13,19 @@ module "shell-kube-credentials" {
   EOH
 }
 
+
+#  Now, we get back the output of the script
+output "shell-kube-credentials-stdout" {
+  value = module.shell-kube-credentials.stdout
+}
+
+#  Now, we get back the output of the script
+output "shell-kube-credentials-stderr" {
+  value = module.shell-kube-credentials.stderr
+}
+
+
+
 module "shell-kube-host" {
   depends_on = [
     module.shell-kube-credentials,
@@ -27,20 +40,16 @@ module "shell-kube-host" {
   EOH
 }
 
-module "shell-debug-host" {
-  depends_on = [
-    module.shell-kube-credentials,
-    module.cluster
-  ]
 
-  source       = "Invicton-Labs/shell-data/external"
-  command_unix = <<EOH
-    mkdir -p ${path.module}/tmpfiles
-    az login --service-principal -u "$ARM_CLIENT_ID" -p "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID"
-    az aro show --name ${var.label} --resource-group ${local.resource_group_name} --query '{api:apiserverProfile.ip, ingress:ingressProfiles[0].ip, consoleUrl:consoleProfile.url, apiUrl:apiserverProfile.url}' --output json
-   EOH
+#  Now, we get back the output of the script
+output "shell-kube-host-stdout" {
+  value = module.shell-kube-host.stdout
 }
 
+#  Now, we get back the output of the script
+output "shell-kube-host-stderr" {
+  value = module.shell-kube-host.stderr
+}
 
 module "shell-oc-login" {
   depends_on = [
@@ -61,6 +70,20 @@ module "shell-oc-login" {
     oc login https://${jsondecode(module.shell-kube-host.stdout)["api"]}:6443/ --insecure-skip-tls-verify=true --username ${jsondecode(module.shell-kube-credentials.stdout)["kubeadminUsername"]} --password ${jsondecode(module.shell-kube-credentials.stdout)["kubeadminPassword"]} > /dev/null
   EOH
 }
+
+
+#  Now, we get back the output of the script
+output "shell-oc-login-stdout" {
+  value = module.shell-oc-login.stdout
+}
+#  Now, we get back the output of the script
+output "shell-oc-login-stderr" {
+  value = module.shell-oc-login.stderr
+}
+
+
+
+
 
 data "local_file" "kubeconfig" {
   depends_on = [
