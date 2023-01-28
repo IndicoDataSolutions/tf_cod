872f9dc4f4dc1f6300ce51dad720f72d8f33e2b9
Eric Fontana <eric.fontana@indico.io>
2023-01-28 12:30:30 -0500

    Debug get token

--- azure/openshift/modules/openshift-cluster/get_token.sh
+++ azure/openshift/modules/openshift-cluster/get_token.sh
@@ -1,1 +1,44 @@
-../../get_token.sh
+#!/bin/bash
+
+name=$1
+resource_group=$2
+kube_config=$3
+
+set -e
+
+#az aro show --name "$1" --resource-group "$2" --query '{api:apiserverProfile.ip, ingress:ingressProfiles[0].ip, url:consoleProfile.url}' 
+az login --service-principal -u "$ARM_CLIENT_ID" -p "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID"
+az aro list-credentials --name "$1" --resource-group "$2" --output json > creds.json
+az aro show --name "$1" --resource-group "$2" --query '{api:apiserverProfile.ip, ingress:ingressProfiles[0].ip, consoleUrl:consoleProfile.url, apiUrl:apiserverProfile.url}' --output json > info.json
+
+username=$(cat creds.json | jq -r '.kubeadminUsername')
+password=$(cat creds.json | jq -r '.kubeadminPassword')
+api_ip=$(cat info.json | jq -r '.api')
+api_url=$(cat info.json | jq -r '.apiUrl')
+
+if [ "${kube_config}" == "" ]; then
+  export KUBECONFIG="/tmp/.openshift_kubeconfig"
+  if [ -f "$KUBECONFIG" ]; then
+    rm $KUBECONFIG
+  fi
+  touch "/tmp/.openshift_kubeconfig"
+else 
+  echo "Updating $KUBECONFIG"
+fi
+oc login $api_url --username ${username} --password ${password}
+os=$(uname -s)
+if [ "$os" == 'Darwin' ]; then
+an_hour_from_now=$(date -v+1H -u '+%Y-%m-%dT%H:%M:%SZ')
+else
+an_hour_from_now=$(date -u -d '+1 hour' '+%Y-%m-%dT%H:%M:%SZ')
+fi
+
+token=$(cat /tmp/.openshift_kubeconfig | yq '.users[0].user.token')
+version='client.authentication.k8s.io/v1beta1'
+json=$( jq -n -c \
+  --arg version "$version" \
+  --arg api_ip "$api_url" \
+  --arg token "$token" \
+  --arg an_hour_from_now "$an_hour_from_now" \
+  '{kind: "ExecCredential", apiVersion: $version, spec: {cluster: {server: $api_ip, "insecure-skip-tls-verify": true}}, status: {expirationTimeStamp: $an_hour_from_now, token: $token}}' )
+echo $json             
