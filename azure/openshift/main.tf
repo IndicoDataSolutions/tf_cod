cab1a11ba573da67edaf75eaff0fd783fd7ed69c
Eric Fontana <eric.fontana@indico.io>
2023-01-28 05:54:21 -0500

    Dont use custom domain for console

--- azure/openshift/main.tf
+++ azure/openshift/main.tf
@@ -275,7 +275,7 @@ module "cluster" {
   ]
   subscriptionId    = split("/", data.azurerm_subscription.primary.id)[2]
   pull_secret       = jsondecode(data.vault_kv_secret_v2.terraform-redhat.data_json)["openshift-pull-secret"]
-  cluster_domain    = lower("${var.label}.${var.account}")
+  cluster_domain    = lower("${var.label}-${var.account}")
   source            = "./modules/openshift-cluster"
   label             = var.label
   region            = var.region
