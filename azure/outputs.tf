29c849a7a57d94b06d1aa1390cbbaa46c2c8ce79
Andrey Chmykh <90482061+ashmuck@users.noreply.github.com>
2023-01-30 14:39:15 -0500

    Added ssh key output for the cluster manager

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,6 +1,10 @@
 output "cluster_manager_ip" {
   value = module.cluster-manager.cluster_manager_ip
 }
+  
+output "cluster_manager_key" {
+  value = tls_private_key.pk.public_key_openssh 
+}
 
 output "terraform_ip" {
   value = local.current_ip
