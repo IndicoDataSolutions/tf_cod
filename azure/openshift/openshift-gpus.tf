6023c9226d178c65415a5c2e047d2e9525f57b02
Eric Fontana <eric.fontana@indico.io>
2023-02-08 09:16:14 -0500

    Read GPU values

---
+++ azure/openshift/openshift-gpus.tf
@@ -0,0 +1,49 @@
+
+
+resource "kubernetes_namespace" "gpu" {
+  depends_on = [
+    module.cluster
+  ]
+  metadata {
+    labels = {
+      "indico.io/openshift" = "true"
+    }
+    name = "nvidia-gpu-operator"
+  }
+}
+
+resource "kubernetes_manifest" "gpu" {
+  depends_on = [
+    kubernetes_namespace.gpu
+  ]
+
+  manifest = {
+    apiVersion = "operators.coreos.com/v1"
+    kind       = "OperatorGroup"
+    metadata = {
+      name      = "nvidia-gpu-operator-group"
+      namespace = "nvidia-gpu-operator"
+    }
+  }
+}
+
+data "kubernetes_resource" "package" {
+  api_version = "packages.operators.coreos.com/v1"
+  kind        = "PackageManifest"
+
+  metadata {
+    name      = "gpu-operator-certified"
+    namespace = "openshift-marketplace"
+  }
+}
+
+output "channel" {
+  value = data.kubernetes_resource.package.object.status.defaultChannel
+}
+
+
+output "status" {
+  value = data.kubernetes_resource.package.object.status
+}
+
+
