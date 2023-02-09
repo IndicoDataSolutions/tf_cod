0c264cc8c0a4eaec76bd3e5e1a3b30b56fe72758
Eric Fontana <eric.fontana@indico.io>
2023-02-09 05:09:17 -0500

    Added NFD operator

--- azure/openshift/openshift-gpus.tf
+++ azure/openshift/openshift-gpus.tf
@@ -1,5 +1,23 @@
 
 
+
+locals {
+  nfd_namespace             = "openshift-nfd"
+  nvidia_operator_namespace = "nvidia-gpu-operator"
+  package                   = element([for c in data.kubernetes_resource.package.object.status.channels : c.currentCSV if c.name == data.kubernetes_resource.package.object.status.defaultChannel], 0)
+  channel                   = data.kubernetes_resource.package.object.status.defaultChannel
+}
+
+output "channel" {
+  value = local.channel
+}
+
+# PACKAGE=$(oc get packagemanifests/gpu-operator-certified -n openshift-marketplace -ojson | jq -r '.status.channels[] | select(.name == "'$CHANNEL'") | .currentCSV')
+output "package" {
+  value = local.package
+}
+
+
 resource "kubernetes_namespace" "gpu" {
   depends_on = [
     module.cluster
@@ -41,24 +59,6 @@ data "kubernetes_resource" "package" {
   }
 }
 
-/*
-envsubst  <<EOF | oc apply -f -
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: gpu-operator-certified
-  namespace: nvidia-gpu-operator
-spec:
-  channel: "$CHANNEL"
-  installPlanApproval: Automatic
-  name: gpu-operator-certified
-  source: certified-operators
-  sourceNamespace: openshift-marketplace
-  startingCSV: "$PACKAGE"
-EOF
-*/
-
-
 resource "kubernetes_manifest" "gpu-operator-subscription" {
   depends_on = [
     kubernetes_namespace.gpu
@@ -90,17 +90,66 @@ resource "kubernetes_manifest" "gpu-operator-subscription" {
 }
 
 
-locals {
-  nvidia_operator_namespace = "nvidia-gpu-operator"
-  package                   = element([for c in data.kubernetes_resource.package.object.status.channels : c.currentCSV if c.name == data.kubernetes_resource.package.object.status.defaultChannel], 0)
-  channel                   = data.kubernetes_resource.package.object.status.defaultChannel
+resource "kubernetes_namespace" "nfd" {
+  depends_on = [
+    kubernetes_manifest.gpu-operator-subscription
+  ]
+
+  metadata {
+    labels = {
+      "indico.io/openshift" = "true"
+    }
+    name = local.nfd_namespace
+  }
 }
 
-output "channel" {
-  value = local.channel
+
+resource "kubernetes_manifest" "nfd" {
+  depends_on = [
+    kubernetes_namespace.nfd
+  ]
+
+  manifest = {
+    apiVersion = "operators.coreos.com/v1"
+    kind       = "OperatorGroup"
+    metadata = {
+      generateName = "openshift-nfd-"
+      name         = "openshift-nfd"
+      namespace    = local.nfd_namespace
+    }
+  }
 }
 
-# PACKAGE=$(oc get packagemanifests/gpu-operator-certified -n openshift-marketplace -ojson | jq -r '.status.channels[] | select(.name == "'$CHANNEL'") | .currentCSV')
-output "package" {
-  value = local.package
+
+resource "kubernetes_manifest" "nfd-operator-subscription" {
+  depends_on = [
+    kubernetes_namespace.nfd
+  ]
+
+  manifest = {
+    apiVersion = "operators.coreos.com/v1alpha1"
+    kind       = "Subscription"
+    metadata = {
+      name      = "nfd"
+      namespace = local.nfd_namespace
+    }
+    spec = {
+      channel             = "stable"
+      installPlanApproval = "Automatic"
+      name                = "nfd"
+      source              = "redhat-operators"
+      sourceNamespace     = "openshift-marketplace"
+    }
+  }
+  # wait until ready
+  wait {
+    fields = {
+      "status.conditions[0].type"   = "CatalogSourcesUnhealthy"
+      "status.conditions[0].status" = "False"
+    }
+  }
 }
+
+
+
+
