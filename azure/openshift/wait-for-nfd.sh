3466d3eff780e04331ee30280b1493f154feb3f9
Eric Fontana <eric.fontana@indico.io>
2023-02-10 08:25:31 -0500

    Wait for ready on NFD

--- azure/openshift/wait-for-nfd.sh
+++ azure/openshift/wait-for-nfd.sh
@@ -2,15 +2,15 @@
 namespace=$1
 subscription=$2
 
-#set -x
+set -x
 ready="false"
 retry_attempts=60
 healthy="Available"
 
-until [ $ready == $healthy ] || [ $retry_attempts -le 0 ]
+until [ "$ready" == "$healthy" ] || [ $retry_attempts -le 0 ]
 do
   ready=$(kubectl get nodefeaturediscovery -n $1 $2 -o json | jq -r '.status.conditions[] | select((.status == "True") and (.type == "Available")) | .type')
-  if [ $ready != $healthy ]; then
+  if [ "$ready" != "$healthy" ]; then
     echo "Not ready, waiting ${retry_attempts} ${ready}"
     sleep 30
     ((retry_attempts--))
