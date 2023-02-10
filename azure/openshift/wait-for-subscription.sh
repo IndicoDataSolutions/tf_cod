75e398118d9053a5f5e35c50e2c66cb57f3021b9
Eric Fontana <eric.fontana@indico.io>
2023-02-10 05:44:39 -0500

    Fixed wait script

--- azure/openshift/wait-for-subscription.sh
+++ azure/openshift/wait-for-subscription.sh
@@ -2,17 +2,18 @@
 namespace=$1
 subscription=$2
 
-set -x
+#set -x
 ready="false"
 retry_attempts=60
 healthy="AllCatalogSourcesHealthy"
 
 until [ $ready == $healthy ] || [ $retry_attempts -le 0 ]
 do
-  ready=$(kubectl get subscription -n $1 $2 -o json | jq -r '.items[0].status.conditions[] | select((.status == "False") and (.type == "CatalogSourcesUnhealthy")) | .reason')
+  ready=$(kubectl get subscription -n $1 $2 -o json | jq -r '.status.conditions[] | select((.status == "False") and (.type == "CatalogSourcesUnhealthy")) | .reason')
   if [ $ready != $healthy ]; then
-    echo "Not ready, sleeping 3"
-    sleep 3
+    echo "Not ready, waiting ${retry_attempts} ${ready}"
+    sleep 30
+    ((retry_attempts--))
   else
     echo "$1/$2 is ready"
   fi
