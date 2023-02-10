c28993613c9bb990addde68b7e93bb0aa68e0a84
Eric Fontana <eric.fontana@indico.io>
2023-02-10 07:10:02 -0500

    Update CRDs to support openshift reloader

--- azure/openshift/openshift-variables.tf
+++ azure/openshift/openshift-variables.tf
@@ -43,7 +43,7 @@ variable "roles" {
 }
 variable "ipa_openshift_crds_version" {
   type    = string
-  default = "0.1.1-add-openshift-crds-41b83ab3"
+  default = "0.1.1-add-openshift-crds-b7b35917"
 }
 
 variable "kubernetes_host" {
