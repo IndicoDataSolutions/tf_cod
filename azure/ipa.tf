3b529085363625fb2567388ec01a673612f11896
Andrey Chmykh <andrey.chmykh@indico.io>
2023-03-03 17:05:32 -0500

    Added space to retrigger tulip plan

--- azure/ipa.tf
+++ azure/ipa.tf
@@ -379,7 +379,7 @@ spec:
         - name: HELM_TF_COD_VALUES
           value: |
             aws-node-termination:
-              enabled: false
+              enabled: false 
             global:
               podLabels:
                 "azure.workload.identity/use": "true"
