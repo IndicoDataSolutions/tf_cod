f168f411379adc5bf6fae0078c13ba03bef308bb
NathanOkolita <nathan.okolita@gmail.com>
2023-03-09 09:15:35 -0500

    [DOP-1477][DOP-1316][DOP-1428] Change order

--- ipa.tf
+++ ipa.tf
@@ -106,16 +106,16 @@ clusterIssuer:
   - dns01:
       route53:
         region: ${var.region}
-    selector:
-      matchLabels:
-        "acme.cert-manager.io/dns01-solver": "true"
-  - dns01:
-      route53:
-        region: ${var.region}
         role: ${var.aws_primary_dns_role_arn}
     selector:
       matchLabels:
         "acme.cert-manager.io/dns02-solver": "true"
+  - dns01:
+      route53:
+        region: ${var.region}
+    selector:
+      matchLabels:
+        "acme.cert-manager.io/dns01-solver": "true"
 alternate-external-dns:
   enabled: true
   logLevel: debug
