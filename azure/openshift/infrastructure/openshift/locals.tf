08c2342986e2397fe245b4a6880903a229621bcd
Eric Fontana <eric.fontana@indico.io>
2023-03-13 12:00:25 -0400

    remove extra comma

--- azure/openshift/infrastructure/openshift/locals.tf
+++ azure/openshift/infrastructure/openshift/locals.tf
@@ -16,7 +16,7 @@ locals {
                 "email": ["${var.openid_emailclaim}"], 
                 "groups": ["${var.openid_groups_claim}"],
                 "name": ["name"],
-                "preferredUsername": ["${var.openid_preferred_username}"],
+                "preferredUsername": ["${var.openid_preferred_username}"]
               },
               "clientID": "${var.openid_client_id}",
               "clientSecret": {
