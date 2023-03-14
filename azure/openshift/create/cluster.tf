0d5a9cfacecaf7a8cc6fc954afeb452fa22ff7b4
Eric Fontana <eric.fontana@indico.io>
2023-03-14 06:35:46 -0400

    Fixed vault path for k8s creds

--- azure/openshift/create/cluster.tf
+++ azure/openshift/create/cluster.tf
@@ -210,7 +210,7 @@ resource "vault_kv_secret_v2" "terraform-credentials" {
     data.kubernetes_secret_v1.terraform
   ]
   mount = var.vault_mount
-  name  = lower("${var.account}-${var.region}-${var.label}")
+  name  = lower("${var.account}-${var.region}-${var.label}-k8s")
 
   data_json = jsonencode(
     {
