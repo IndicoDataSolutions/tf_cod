797d52345ed92b4d6468b020f265ead445c05f1a
Eric Fontana <eric.fontana@indico.io>
2023-03-16 12:33:04 -0400

    Removed extra trailing slash

--- azure/openshift/infrastructure/openshift/locals.tf
+++ azure/openshift/infrastructure/openshift/locals.tf
@@ -4,7 +4,7 @@ locals {
   # 
   # to: https://oauth-openshift.apps.dop1487-indico-dev-azure.eastus.aroapp.io/oauth2callback/openid
   callback_host = replace(var.openshift_console_url, "console-openshift-console", "oauth-openshift")
-  callback_url  = "${local.callback_host}/oauth2callback/openid"
+  callback_url  = "${local.callback_host}oauth2callback/openid"
   openid_name   = "openid"
   openid_cluster_patch = jsonencode(<<JSON
     [
