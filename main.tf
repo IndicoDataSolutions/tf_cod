2ac88cc15f01a987f1158fc600d4cc60a4bbfbc5
Eric Fontana <eric.fontana@indico.io>
2023-03-30 11:41:37 -0400

    Load the key from vault

--- main.tf
+++ main.tf
@@ -293,9 +293,6 @@ module "cluster" {
 }
 
 module "snowflake" {
-  depends_on = [
-    data.vault_kv_secret_v2.snowflake-credentials
-  ]
   source                = "app.terraform.io/indico/indico-aws-snowflake/mod"
   label                 = var.label
   additional_tags       = var.additional_tags
