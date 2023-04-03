97d079022c3c07f1346301d1af5cbe9ec2c1b83e
indico-devops-sa <andrey.chmykh@indicodata.ai>
2023-04-03 13:31:00 -0400

    Added dependency (though it might have been implicit)

--- monitoring.tf
+++ monitoring.tf
@@ -205,6 +205,9 @@ resource "helm_release" "opentelemetry-collector" {
 }
 
 resource "kubectl_manifest" "pod-security-admission-controller" {
+  depends_on = [
+    module.cluster
+  ]
   count = var.enable_pod_security == "true" ? 1 : 0
   yaml_body = <<YAML
 apiVersion: apiserver.config.k8s.io/v1 # see compatibility note
