b8e76fec9be5d67806f4a7455d07b27353db4ab8
Dov Turner <dov.turner@indico.io>
2023-05-18 12:59:00 -0500

    add in 5.9.3 changes

--- variables.tf
+++ variables.tf
@@ -509,3 +509,8 @@ variable "use_acm" {
   description = "create cluster that will use acm"
 }
 
+variable "aws_primary_dns_role_arn" {
+  type        = string
+  default     = ""
+  description = "The AWS arn for the role needed to manage route53 DNS in a different account."
+}
