398dcc428f15eef354e4686351a10ce158b5da23
Andrey Chmykh <andrey.chmykh@indico.io>
2023-06-14 16:42:52 -0400

    dop-1670 (#144)

--- azure/openshift/variables.tf
+++ azure/openshift/variables.tf
@@ -214,7 +214,7 @@ variable "svp_client_secret" {
 
 variable "k8s_version" {
   type        = string
-  default     = "1.23.12"
+  default     = "1.24.10"
   description = "The version of the kubernetes cluster"
 }
 
