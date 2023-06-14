75c3a6771f83e91916408d35796908267e0bcfc2
Andrey Chmykh <andrey.chmykh@indico.io>
2023-06-14 10:19:57 -0400

    Updated argo (#140)

--- azure/ipa.tf
+++ azure/ipa.tf
@@ -609,7 +609,7 @@ resource "argocd_application" "ipa" {
     }
 
     sync_policy {
-      automated = {
+      automated {
         prune       = true
         self_heal   = false
         allow_empty = false
--- azure/openshift/create/cluster.tf
+++ azure/openshift/create/cluster.tf
@@ -44,7 +44,7 @@ module "argo-registration" {
   count = var.argo_enabled == true ? 1 : 0
 
   source                       = "app.terraform.io/indico/indico-argo-registration/mod"
-  version                      = "1.1.12"
+  version                      = "1.1.16"
   cluster_name                 = var.label
   region                       = var.region
   argo_password                = var.argo_password
