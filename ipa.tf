1bae7da99e51e832da22138a492bc54afb36d3e7
indico-devops-sa <andrey.chmykh@indicodata.ai>
2023-06-26 16:38:54 -0400

    Added dns resolver

--- ipa.tf
+++ ipa.tf
@@ -273,7 +273,16 @@ secrets:
       eabEmail: devops-sa@indico.io
       eabKid: "${jsondecode(data.vault_kv_secret_v2.zerossl_data.data_json)["EAB_KID"]}"
       eabHmacKey: "${jsondecode(data.vault_kv_secret_v2.zerossl_data.data_json)["EAB_HMAC_KEY"]}"
-     
+
+clusterIssuer:
+  additionalSolvers:
+    - dns01:
+        route53:
+          region: ${var.region}
+      selector:
+        matchLabels:
+          "acme.cert-manager.io/dns01-solver": "true"
+
 monitoring:
   enabled: true
   global:
