6af2e2e062299d3a5d49b147b5d802e699baccbb
NathanOkolita <nathan.okolita@gmail.com>
2023-06-30 05:51:31 -0400

    [DOP-1706] Create VPC endpoint

--- aws_specific_modules.tf
+++ aws_specific_modules.tf
@@ -22,6 +22,85 @@ module "k8s_dashboard" {
   keycloak_client_secret = module.keycloak.client_secret
 }
 
+data "aws_vpc_endpoint_service" "guardduty" {
+  service_type = "Interface"
+  filter {
+    name   = "service-name"
+    values = ["com.amazonaws.${data.aws_region.current.name}.guardduty-data"]
+  }
+}
+
+resource "aws_vpc_endpoint" "eks_vpc_guardduty" {
+  vpc_id            = module.vpc_eks.vpc_id
+  service_name      = data.aws_vpc_endpoint_service.guardduty.service_name
+  vpc_endpoint_type = "Interface"
+  
+  policy = data.aws_iam_policy_document.eks_vpc_guardduty.json
+
+  security_group_ids  = [aws_security_group.eks_vpc_endpoint_guardduty.id]
+  subnet_ids          = local.network[0].public_subnet_ids
+  private_dns_enabled = true
+}
+
+resource "aws_security_group" "eks_vpc_endpoint_guardduty" {
+  name_prefix = "${local.name_prefix}-vpc-endpoint-guardduty-sg-"
+  description = "Security Group used by VPC Endpoints."
+  vpc_id      = ${local.network[0].indico_vpc_id}
+
+  tags = {
+    "Name"             = "${local.name_prefix}-vpc-endpoint-guardduty-sg-"
+    "GuardDutyManaged" = "false"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "eks_vpc_guardduty_ingress" {
+  security_group_id = aws_security_group.eks_vpc_endpoint_guardduty.id
+  description       = "Ingress for port 443."
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+data "aws_iam_policy_document" "eks_vpc_guardduty" {
+  statement {
+    actions = ["*"]
+
+    effect = "Allow"
+
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+
+  statement {
+    actions = ["*"]
+
+    effect = "Deny"
+
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:PrincipalAccount"
+
+      values = [data.aws_caller_identity.current.account_id]
+    }
+  }
+}
 resource "aws_eks_addon" "guardduty" {
   depends_on = [
     module.cluster
