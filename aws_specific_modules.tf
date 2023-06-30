6c5b2f443319117338b644a16c910f474e419de9
NathanOkolita <nathan.okolita@gmail.com>
2023-06-30 06:00:52 -0400

    [DOP-1706]Fix syntax

--- aws_specific_modules.tf
+++ aws_specific_modules.tf
@@ -45,7 +45,7 @@ resource "aws_vpc_endpoint" "eks_vpc_guardduty" {
 resource "aws_security_group" "eks_vpc_endpoint_guardduty" {
   name_prefix = "${local.name_prefix}-vpc-endpoint-guardduty-sg-"
   description = "Security Group used by VPC Endpoints."
-  vpc_id      = ${local.network[0].indico_vpc_id}
+  vpc_id      = local.network[0].indico_vpc_id
 
   tags = {
     "Name"             = "${local.name_prefix}-vpc-endpoint-guardduty-sg-"
