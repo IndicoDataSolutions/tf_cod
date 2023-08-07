e74cc7a669623decdc49fd2b4f5087636f2066f6
NathanOkolita <nathan.okolita@gmail.com>
2023-08-07 07:56:56 -0400

    update to use indico-azure-cluster version 3.0.0 to use standard AKS tier instead of free.

--- azure/main.tf
+++ azure/main.tf
@@ -203,7 +203,7 @@ module "cluster" {
     azurerm_resource_group.cod-cluster
   ]
   source                  = "app.terraform.io/indico/indico-azure-cluster/mod"
-  version                 = "2.0.22"
+  version                 = "3.0.0"
   label                   = var.label
   public_key              = tls_private_key.pk.public_key_openssh
   region                  = var.region
