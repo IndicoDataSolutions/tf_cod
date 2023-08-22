a77d49c87e40de32219516eb6b631c1f0a5b8fa9
NathanOkolita <nathan.okolita@gmail.com>
2023-08-22 07:04:46 -0400

    [DOP-1717] Update Azure to use standard tier not free.

--- azure/main.tf
+++ azure/main.tf
@@ -219,7 +219,7 @@ module "cluster" {
 
   source                     = "app.terraform.io/indico/indico-azure-cluster/mod"
   insights_retention_in_days = var.monitor_retention_in_days
-  version                    = "2.0.28"
+  version                    = "3.0.0"
   label                      = var.label
   public_key                 = tls_private_key.pk.public_key_openssh
   region                     = var.region
