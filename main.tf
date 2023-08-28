514a09c34e8ffcea4d9dedbcb734d7b0ba4d9235
indico-devops-sa <andrey.chmykh@indicodata.ai>
2023-08-28 12:33:30 -0400

    Update providers

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.38.0"
+      version = "~> 5.14.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.7.2"
+      version = "0.9.1"
     }
     keycloak = {
       source  = "mrparkers/keycloak"
