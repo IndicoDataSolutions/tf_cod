8a7fbd885a706c24afa86f9c4e9cc87a5d3ac72e
indico-devops-sa <andrey.chmykh@indicodata.ai>
2023-08-28 16:11:23 -0400

    Update module for cluster

--- main.tf
+++ main.tf
@@ -276,7 +276,7 @@ module "cluster" {
   aws_account_name           = var.aws_account
   oidc_enabled               = false
   source                     = "app.terraform.io/indico/indico-aws-eks-cluster/mod"
-  version                    = "8.0.3"
+  version                    = "8.0.4"
   label                      = var.label
   additional_tags            = var.additional_tags
   region                     = var.region
