72a3c69a37497d9f96fda3e2479a570db1dcbebe
indico-devops-sa <andrey.chmykh@indicodata.ai>
2023-08-30 10:30:21 -0400

    Updating the cluster module to revert the metrics change in 3.1.2

--- azure/main.tf
+++ azure/main.tf
@@ -219,7 +219,7 @@ module "cluster" {
 
   source                     = "app.terraform.io/indico/indico-azure-cluster/mod"
   insights_retention_in_days = var.monitor_retention_in_days
-  version                    = "3.1.2"
+  version                    = "3.1.3"
   label                      = var.label
   public_key                 = tls_private_key.pk.public_key_openssh
   region                     = var.region
