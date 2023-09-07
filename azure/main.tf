59dc4bffdab9d5bb80549e6dedc1820504087abe
Eric Fontana <eric.fontana@indico.io>
2023-09-07 10:57:42 -0400

    Unique file storage account name

--- azure/main.tf
+++ azure/main.tf
@@ -190,7 +190,7 @@ module "storage" {
     azurerm_resource_group.cod-cluster
   ]
   source                     = "app.terraform.io/indico/indico-azure-blob/mod"
-  version                    = "0.1.21"
+  version                    = "0.1.22"
   label                      = var.label
   region                     = var.region
   resource_group_name        = local.resource_group_name
