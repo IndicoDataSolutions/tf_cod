25289b74d01f0c2ee5e7352c7f500ecf4eb6841f
Nathan Okolita <nathanokolita@Nathans-MacBook-Pro.local>
2023-09-14 11:02:21 -0400

    [DOP-1861] Add second solver to support using alternate domain on azure cluster.

--- azure/openshift/ipa-testing/required-providers.tf
+++ azure/openshift/ipa-testing/required-providers.tf
@@ -1,1 +1,47 @@
-../required-providers.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.40.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.33.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">=1.2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.17.0"
+    }
+    kubectl = {
+      source = "gavinbunney/kubectl"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+    argocd = {
+      source  = "oboukili/argocd"
+      version = "5.4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "=2.2.3"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "4.26.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.13.0"
+    }
+  }
+}
