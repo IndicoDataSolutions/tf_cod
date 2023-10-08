770f6f726cbdcdf6fb6983325e129e6fb630bbf1
aqeelin <aqeel.abbas@indico.io>
2023-10-07 18:54:44 -0600

    only adding efs and alb

---
+++ ipa.tf
@@ -0,0 +1,29 @@
+resource "helm_release" "ipa-pre-requisites" {
+  depends_on = [
+    module.cluster
+  ]
+
+  verify           = false
+  name             = "ipa-pre-reqs"
+  create_namespace = true
+  namespace        = "default"
+  repository       = var.ipa_repo
+  chart            = "ipa-pre-requisites"
+  version          = var.ipa_pre_reqs_version
+  wait             = false
+  timeout          = "1800" # 30 minutes
+  disable_webhooks = false
+
+  values = [<<EOF
+  aws-fsx-csi-driver:
+    enabled: false
+  aws-efs-csi-driver:
+    enabled: true
+  aws-load-balancer-controller:
+    enabled: true
+  ingress:
+    enabled: true
+      
+ EOF
+  ]
+}
