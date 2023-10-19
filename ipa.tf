d9d6f0254d4464cb3d9990384ee94ca2f862cf47
Eric Fontana <eric.fontana@indico.io>
2023-10-19 11:29:37 -0400

    Updated tag to use the sha

--- ipa.tf
+++ ipa.tf
@@ -645,7 +645,7 @@ resource "helm_release" "terraform-smoketests" {
     region: ${var.region}
     name: ${var.label}
   image:
-    tag: 
+    tag: ${substr(data.external.git_information.result.sha, 0, 8)}
   EOF
   ]
 }
