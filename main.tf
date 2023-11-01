200c874c6f2a8c242008836cf91df01cac726d17
NathanOkolita <Nathan.Okolita@indicodata.ai>
2023-11-01 06:02:32 -0400

    [DOP-1957] revert keycloak settings

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     }
     keycloak = {
       source  = "mrparkers/keycloak"
-      version = "4.0.1"
+      version = "4.3.1"
     }
     argocd = {
       source  = "oboukili/argocd"
@@ -50,8 +50,7 @@ terraform {
 provider "time" {}
 
 provider "keycloak" {
-  client_id = "terraform-master"
-  url       = "https://keycloak.devops.indico.io"
+  # these values are provided by the keycloak varset from terraform cloud
 }
 
 provider "vault" {
