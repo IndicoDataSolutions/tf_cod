7f00bf76453ba5cf0da4339240d54eafa3402f79
Eric Fontana <eric.fontana@indico.io>
2023-11-03 13:27:11 -0400

    Setup default auth

--- modules/common/vault-secrets-operator-setup/variables.tf
+++ modules/common/vault-secrets-operator-setup/variables.tf
@@ -16,19 +16,15 @@ output "vault_mount_path" {
   value = local.account_region_name
 }
 
-
 output "vault_auth_role_name" {
   value = vault_kubernetes_auth_backend_role.vault-auth-role.role_name
 }
-
 
 output "vault_auth_service_account_name" {
   value = kubernetes_service_account_v1.vault-auth.metadata.0.name
 }
 
-
-
-output "vault_auth_audiences" {
-  value = [var.audience]
+output "vault_auth_audience" {
+  value = var.audience
 }
 
