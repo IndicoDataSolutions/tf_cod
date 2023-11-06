08a27012dea3f7cad30e42c57359a223181a5e29
NathanOkolita <Nathan.Okolita@indicodata.ai>
2023-11-06 06:52:14 -0500

    [DOP-1957] include keycloak fix.

--- modules/aws/keycloak/keycloak.tf
+++ modules/aws/keycloak/keycloak.tf
@@ -28,7 +28,7 @@ resource "null_resource" "register-callback" {
   # must use full resource def for the apply, but must use self.triggers for destroy
   # annoying: https://stackoverflow.com/questions/72820832/self-triggers-is-null-when-trying-to-use-trigger-values-in-local-exec-provisio
   provisioner "local-exec" {
-    command = "curl -XPOST -H 'Content-Type: application/json' -H \"Authorization: Bearer ${null_resource.register-callback.triggers.client_secret}\" -v https://keycloak-service.devops.indico.io/add --data '{\"url\": \"https://k8s.${null_resource.register-callback.triggers.dns_name}/oauth2/callback\"}'"
+    command = "curl -XPOST -H 'Content-Type: application/json' -H \"Authorization: Bearer ${data.keycloak_openid_client.kube-oidc-proxy.client_secret}\" -v https://keycloak-service.devops.indico.io/add --data '{\"url\": \"https://k8s.${var.local_dns_name}/oauth2/callback\"}'"
   }
 
   provisioner "local-exec" {
