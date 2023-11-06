05c9035146ee9474031c993b0f18b84273407e1b
Eric Fontana <eric.fontana@indico.io>
2023-11-06 10:14:27 -0500

    Moved outputs to outputs.tf

--- modules/common/vault-secrets-operator-setup/variables.tf
+++ modules/common/vault-secrets-operator-setup/variables.tf
@@ -11,20 +11,3 @@ variable "audience" {
 locals {
   account_region_name = lower("${var.account}-${var.region}-${var.name}")
 }
-
-output "vault_mount_path" {
-  value = local.account_region_name
-}
-
-output "vault_auth_role_name" {
-  value = vault_kubernetes_auth_backend_role.vault-auth-role.role_name
-}
-
-output "vault_auth_service_account_name" {
-  value = kubernetes_service_account_v1.vault-auth.metadata.0.name
-}
-
-output "vault_auth_audience" {
-  value = var.audience
-}
-
