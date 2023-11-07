57e53c3d1d47f180ff32a3c6c0bacdbf66bc9648
Eric Fontana <eric.fontana@indico.io>
2023-11-07 08:05:16 -0500

    Fixed dependencies

--- ipa.tf
+++ ipa.tf
@@ -372,7 +372,7 @@ resource "time_sleep" "wait_1_minutes_after_crds" {
   create_duration = "1m"
 }
 
-
+/*
 resource "kubernetes_manifest" "thanos-storage-secret" {
   depends_on = [helm_release.ipa-crds, module.secrets-operator-setup]
 
@@ -395,7 +395,7 @@ resource "kubernetes_manifest" "thanos-storage-secret" {
     }
   }
 }
-
+*/
 
 resource "helm_release" "ipa-pre-requisites" {
   depends_on = [
