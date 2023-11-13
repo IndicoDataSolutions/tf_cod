3e6cb23e0395a3ee1a603103492813e09f9b3a5a
Eric Fontana <eric.fontana@indico.io>
2023-11-13 16:31:56 -0500

    Added defaults for argo

--- ipa.tf
+++ ipa.tf
@@ -720,6 +720,8 @@ resource "null_resource" "sleep-5-minutes-wait-for-charts-smoketest-build" {
 */
 
 resource "null_resource" "wait-for-tf-cod-chart-build" {
+  count = var.argo_enabled == true ? 1 : 0
+
   depends_on = [
     time_sleep.wait_1_minutes_after_pre_reqs
   ]
@@ -730,7 +732,7 @@ resource "null_resource" "wait-for-tf-cod-chart-build" {
 
   provisioner "local-exec" {
     environment = {
-      HARBOR_API_TOKEN = jsondecode(data.vault_kv_secret_v2.harbor-api-token.data_json)["bearer_token"]
+      HARBOR_API_TOKEN = jsondecode(data.vault_kv_secret_v2.harbor-api-token[0].data_json)["bearer_token"]
     }
     command = "${path.module}/validate_chart.sh terraform-smoketests 0.1.0-${data.external.git_information.result.branch}-${substr(data.external.git_information.result.sha, 0, 8)}"
   }
@@ -779,6 +781,7 @@ data "vault_kv_secret_v2" "account-robot-credentials" {
 
 
 data "vault_kv_secret_v2" "harbor-api-token" {
+  count = var.argo_enabled == true ? 1 : 0
   mount = "tools/argo"
   name  = "harbor-api"
 }
