7a867ee05537ff398de9f1259a126fad833174d5
NathanOkolita <Nathan.Okolita@indicodata.ai>
2023-12-01 07:35:00 -0500

    [DOP-1986] Use SQL-exporter instead of postgres-exporter

--- azure/tf-smoketest-variables.tf
+++ azure/tf-smoketest-variables.tf
@@ -47,6 +47,7 @@ resource "kubernetes_config_map" "terraform-variables" {
     restore_snapshot_name = "${jsonencode(var.restore_snapshot_name)}"
     monitoring_enabled = "${jsonencode(var.monitoring_enabled)}"
     keda_version = "${jsonencode(var.keda_version)}"
+    external_secrets_version = "${jsonencode(var.external_secrets_version)}"
     opentelemetry_collector_version = "${jsonencode(var.opentelemetry-collector_version)}"
     ipa_smoketest_values = "${jsonencode(var.ipa_smoketest_values)}"
     ipa_smoketest_repo = "${jsonencode(var.ipa_smoketest_repo)}"
