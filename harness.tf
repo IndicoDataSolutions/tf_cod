054bd2ec97eb38026a7680ef304261744543e899
Sung96kim <sung96kim@gmail.com>
2023-12-01 14:04:53 -0500

    FIX: str

--- harness.tf
+++ harness.tf
@@ -5,7 +5,7 @@ module "delegate" {
 
   account_id       = jsondecode(data.vault_kv_secret_v2.delegate_secrets.data_json)["DELEGATE_ACCOUNT_ID"]
   delegate_token   = jsondecode(data.vault_kv_secret_v2.delegate_secrets.data_json)["DELEGATE_TOKEN"]
-  delegate_name    = var.cluster_name + "-harness-delegate"
+  delegate_name    = "${var.cluster_name}-harness-delegate"
   namespace        = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io/gratis"
   delegate_image   = jsondecode(data.vault_kv_secret_v2.delegate_secrets.data_json)["DELEGATE_IMAGE"]
