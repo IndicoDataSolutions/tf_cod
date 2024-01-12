8ab4196f5d8af106fddc13f1feed0d2b7f17e451
Andrey Chmykh <andrey.chmykh@indico.io>
2024-01-12 12:11:54 -0500

    Update ipa.tf

--- ipa.tf
+++ ipa.tf
@@ -426,6 +426,7 @@ resource "helm_release" "ipa-crds" {
   chart            = "ipa-crds"
   version          = var.ipa_crds_version
   wait             = true
+  timeout          = "1800" # 30 minutes
 
   values = [
     <<EOF
