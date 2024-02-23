fb4effe51f7b56e2c3b04446b251eab7def3c5d4
NathanOkolita <Nathan.Okolita@indicodata.ai>
2024-02-23 13:30:09 -0500

    [dop-2121] Update AWS version to support updated python version.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.38.0"
+      version = "~> 5.14.0"
     }
     time = {
       source  = "hashicorp/time"
