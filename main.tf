aca3fed396c977c17835b78e860d6f5c9e9c185d
Nathan Okolita <Nathan.Okolita@gmail.com>
2024-02-23 15:02:54 -0500

    [dop-2121] Update FSx module version to use newer python version of lambda (#247)

--- main.tf
+++ main.tf
@@ -280,7 +280,7 @@ module "efs-storage-local-registry" {
 module "fsx-storage" {
   count                       = var.include_fsx == true ? 1 : 0
   source                      = "app.terraform.io/indico/indico-aws-fsx/mod"
-  version                     = "1.4.1"
+  version                     = "1.4.2"
   label                       = var.label
   additional_tags             = var.additional_tags
   region                      = var.region
