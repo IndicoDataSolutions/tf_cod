32015714346855915665e833b4872601d454b583
Nathan Okolita <Nathan.Okolita@gmail.com>
2024-03-18 13:43:01 -0400

    [DOP-2164] Fix issue with readapi queue (#256)
    * [DOP-2164] Pin azurerm to the 3.95.0 previous version.
    
    * [DOP-2164] Pin azurerm to the 3.96.0 current latest version.
    
    * [DOP-2164] Pin azurerm to the 3.95.0 previous version.
    
    * [DOP-2164] Pin azurerm to the 3.95.0 previous version in azure terraform as well

--- azure/main.tf
+++ azure/main.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_providers {
+  required_providers {   
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.40.0"
+      source = "hashicorp/azurerm"
+      version = "3.95.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
