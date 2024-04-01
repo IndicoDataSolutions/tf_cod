68424af212afe9af788ab174cc52f2585e6d17ff
NathanOkolita <Nathan.Okolita@indicodata.ai>
2024-04-01 14:01:00 -0400

    [DOP-2137][DOP-2180] Update eks module 8.2.0.

--- main.tf
+++ main.tf
@@ -311,7 +311,7 @@ module "cluster" {
   aws_account_name           = var.aws_account
   oidc_enabled               = false
   source                     = "app.terraform.io/indico/indico-aws-eks-cluster/mod"
-  version                    = "9.0.0"
+  version                    = "8.2.0"
   label                      = var.label
   additional_tags            = var.additional_tags
   region                     = var.region
