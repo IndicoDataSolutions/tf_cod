b4a7b3f0877a71183b9d5d0ed611e5aa0e0a3171
Eric Fontana <eric.fontana@indico.io>
2024-04-18 14:22:35 -0400

    Pass in the network plugin mode

--- azure/main.tf
+++ azure/main.tf
@@ -284,7 +284,7 @@ module "cluster" {
 
   source                     = "app.terraform.io/indico/indico-azure-cluster/mod"
   insights_retention_in_days = var.monitor_retention_in_days
-  version                    = "4.0.0"
+  version                    = "4.0.1"
   label                      = var.label
   public_key                 = tls_private_key.pk.public_key_openssh
   region                     = var.region
