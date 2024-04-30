a63a9222d670aa27600d5b034e8af99c64097951
Eric Fontana <eric.fontana@indico.io>
2024-04-30 07:12:35 -0400

    Update k8s_dashboard.tf

--- modules/aws/k8s_dashboard/k8s_dashboard.tf
+++ modules/aws/k8s_dashboard/k8s_dashboard.tf
@@ -73,6 +73,7 @@ oauth2-proxy:
   extraArgs:
     insecure-oidc-allow-unverified-email: true
     email-domain: indico.io
+
   redis:
     enabled: true
     replica:
@@ -166,99 +167,7 @@ kubernetes-dashboard:
     disableAccessDeniedNotifications: false
 
 ${local.ingress_values}
-  redis:
-    enabled: true
-    replica:
-      replicaCount: 1
-    affinity:
-      podAntiAffinity:
-          preferredDuringSchedulingIgnoredDuringExecution:
-            - podAffinityTerm:
-                labelSelector:
-                  matchLabels:
-                    app.kubernetes.io/name: redis
-                    app.kubernetes.io/component: replica
-                namespaces:
-                  - "default"
-                topologyKey: kubernetes.io/hostname
-              weight: 1
 
-  sessionStorage:
-    type: redis
-
-  # important
-  extraEnv:
-    - name: OAUTH2_PROXY_OIDC_ISSUER_URL
-      value: https://keycloak.devops.indico.io/auth/realms/GoogleAuth
-    - name: OAUTH2_PROXY_REDIRECT_URL
-      value: https://k8s-${var.local_dns_name}/oauth2/callback
-    - name: OAUTH2_PROXY_PASS_AUTHORIZATION_HEADER
-      value: 'true'
-    - name: OAUTH2_PROXY_EMAIL_DOMAINS
-      value: '*'
-    - name: OAUTH2_PROXY_SKIP_PROVIDER_BUTTON
-      value: 'true'
-    - name: OAUTH2_PROXY_PROVIDER
-      value: 'oidc'
-    - name: OAUTH2_PROXY_ALLOWED_GROUPS
-      value: DevOps,Engineering,QA,Customer Ops
-    - name: OAUTH2_PROXY_UPSTREAMS
-      value: http://k8s-kubernetes-dashboard:443
-    - name: OAUTH2_PROXY_SSL_UPSTREAM_INSECURE_SKIP_VERIFY
-      value: 'true'
-
-  config:
-    clientID: ${var.keycloak_client_id}
-    clientSecret: ${var.keycloak_client_secret}
-   
-
-  service:
-    annotations:
-      external-dns.alpha.kubernetes.io/hostname: k8s-${var.local_dns_name}
-  ingress:
-    enabled: true
-    hosts:
-      - k8s-${var.local_dns_name}
-    annotations:
-      kubernetes.io/ingress.class: nginx
-    tls:
-      - hosts:
-          - k8s-${var.local_dns_name}
-        secretName: ${var.ssl_static_secret_name}
-EOT
-  )
-}
-resource "helm_release" "k8s-dashboard" {
-  name             = "k8s"
-  create_namespace = true
-  namespace        = "default"
-  repository       = var.ipa_repo
-  chart            = "k8s-dashboard"
-  version          = var.k8s_dashboard_chart_version
-  timeout          = 600   # 10 minutes
-  wait             = false # don't bother to wait
-
-  values = [
-    <<EOF
-kubernetes-dashboard:
-  extraArgs:
-  - --enable-insecure-login
-  - --system-banner="Viewing ${var.local_dns_name}"
-  - --token-ttl=43200
-  - --enable-skip-login
-
-  # we are proxying
-  protocolHttp: true
-
-  settings:
-    clusterName: "${var.local_dns_name}"
-    itemsPerPage: 40
-    logsAutoRefreshTimeInterval: 5
-    resourceAutoRefreshTimeInterval: 5
-    disableAccessDeniedNotifications: false
-
-${local.ingress_values}
-
-  EOF
+   EOF
   ]
 }
