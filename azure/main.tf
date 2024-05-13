d4c697efb3c0ce7e62f0a7727f44e574fdb501ac
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-05-13 12:39:57 -0400

    purge resource group on delete (issue with log group

--- azure/main.tf
+++ azure/main.tf
@@ -47,6 +47,9 @@ provider "azurerm" {
     cognitive_account {
       purge_soft_delete_on_destroy = true
     }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
 }
 
