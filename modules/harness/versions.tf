dd031d014e818cacd41ff11d1c559f52e3ac947e
NathanOkolita <Nathan.Okolita@indicodata.ai>
2024-09-23 09:51:56 -0400

    [DOP-2338] adjust pineed helm provider version for module/harness

--- modules/harness/versions.tf
+++ modules/harness/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.11.0"
+      version = "2.15.0"
     }
     utils = {
       source  = "cloudposse/utils"
