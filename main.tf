1698c1f3b1dfe1fca1bf63b20d94f5b32bc7e828
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-11-14 22:48:18 -0500

    Updated aws provider for cluster module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.72.0"
+      version = "5.74.0"
     }
     time = {
       source  = "hashicorp/time"
