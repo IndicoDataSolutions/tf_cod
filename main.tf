d48ec3553aa489672288525ae8952ee5675daacc
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-11-14 22:53:12 -0500

    Updated the bucket name

--- main.tf
+++ main.tf
@@ -302,7 +302,7 @@ module "iam" {
   kms_key_arn = module.kms_key.key_arn
   # Buckets 
   create_bucket = var.bucket_type == "create" 
-  bucket_name = var.bucket_name_override == null ? "indico-data-${var.label}" : var.bucket_name_override
+  data_bucket_name = var.bucket_name_override == null ? "indico-data-${var.label}" : var.bucket_name_override
   include_rox = var.include_rox
   api_models_bucket_name = var.api_models_bucket_name_override == null ? "indico-api-models-${var.label}" : var.api_models_bucket_name_override
   # s3 replication
