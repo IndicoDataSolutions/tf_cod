f309e42241322f4e09dd26a9cfe98db2a71ac0ad
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-11-15 09:36:37 -0500

    Removing all aws_iam_policy_attachment move blocks

--- moved.tf
+++ moved.tf
@@ -12,8 +12,3 @@ moved {
   from = module.cluster.aws_iam_role.node_role
   to   = module.iam.module.create_eks_node_role[0].aws_iam_role.role
 }
-
-moved {
-  from = module.cluster.aws_iam_role_policy_attachment.additional
-  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.additional_policies
-}
