9d6538a8eefa8628e67faa43c77a2252c80051c1
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-11-15 09:27:41 -0500

    Updated the module to better handle state migration

--- moved.tf
+++ moved.tf
@@ -12,3 +12,18 @@ moved {
   from = module.cluster.aws_iam_role.node_role
   to   = module.iam.module.create_eks_node_role[0].aws_iam_role.role
 }
+
+moved {
+  from = module.cluster.aws_iam_role_policy_attachment.additional_cluster_policy
+  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.attachments[0]
+}
+
+moved {
+  from = module.cluster.aws_iam_role_policy_attachment.ebs_cluster_policy
+  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.attachments[1]
+}
+
+moved {
+  from = module.cluster.aws_iam_role_policy_attachment.additional
+  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.additional_policies
+}
