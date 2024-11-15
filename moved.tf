324d9c8d3a2c75c0ef28430e82b5968a14fff57d
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-11-15 09:32:45 -0500

    Issue with the policy attachments?

--- moved.tf
+++ moved.tf
@@ -14,16 +14,6 @@ moved {
 }
 
 moved {
-  from = module.cluster.aws_iam_role_policy_attachment.additional_cluster_policy
-  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.attachments[0]
-}
-
-moved {
-  from = module.cluster.aws_iam_role_policy_attachment.ebs_cluster_policy
-  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.attachments[1]
-}
-
-moved {
   from = module.cluster.aws_iam_role_policy_attachment.additional
   to   = module.iam.module.create_eks_node_role[0].aws_iam_policy_attachment.additional_policies
 }
