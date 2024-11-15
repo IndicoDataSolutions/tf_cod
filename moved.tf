d0a62d0f158da7e42a5a7a1f9161db1ea33222bc
indico-devops-sa <andrey.chmykh@indicodata.ai>
2024-11-15 09:13:14 -0500

    Fixed the state to make it easier to transfer

--- moved.tf
+++ moved.tf
@@ -1,11 +1,11 @@
 moved {
   from = module.cluster.aws_iam_policy.cluster_node_iam_policy
-  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy.policies["${var.label}-ClusterNodePolicy"]
+  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy.policies[0]
 }
 
 moved {
   from = module.cluster.aws_iam_policy.cluster_node_ebs_iam_policy
-  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy.policies["${var.label}-ClusterNodeEBSPolicy"]
+  to   = module.iam.module.create_eks_node_role[0].aws_iam_policy.policies[1]
 }
 
 moved {
