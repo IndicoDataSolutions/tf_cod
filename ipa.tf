e38cb164c7186a8364a47f80048a454d30fda3a3
Lucas Telles <lucas.telles@indicodata.ai>
2024-12-03 11:01:54 -0300

    Wait wait sync from ipa argocd_application

--- ipa.tf
+++ ipa.tf
@@ -76,7 +76,7 @@ locals {
   ] : []
   #storage_spec = var.include_fsx == true ? local.fsx_values : local.efs_values
   storage_spec = var.on_prem_test == true ? local.on_prem_values : var.include_fsx == true ? local.fsx_values : local.efs_values
-  
+
   alb_ipa_values = var.enable_waf == true ? (<<EOT
 app-edge:
   image:
@@ -1555,7 +1555,7 @@ resource "argocd_application" "ipa" {
 
   count = var.argo_enabled == true ? 1 : 0
 
-  wait = false
+  wait = true
 
   metadata {
     name      = lower("${var.aws_account}-${var.region}-${var.label}-deploy-ipa")
