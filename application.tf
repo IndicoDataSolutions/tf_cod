b665d78bf3f9664a22356fd59a564cce3d0a5fad
Lucas Telles <lucas.telles@indicodata.ai>
2025-01-09 12:02:55 -0300

    Try to fix identation

--- application.tf
+++ application.tf
@@ -475,7 +475,7 @@ vault-secrets-operator:
   EOF
   ]
 
-  indico_pre_reqs_values = [<<EOF
+  indico_pre_reqs_values = [<<-EOF
 global:
   host: "${local.dns_name}"
   image:
@@ -670,7 +670,7 @@ keda:
       podMonitor:
         enabled: true
 kube-prometheus-stack:
-  ${local.kube_prometheus_stack_values}
+${local.kube_prometheus_stack_values}
 metrics-server:
   global:
     imageRegistry: ${var.image_registry}/docker.io
