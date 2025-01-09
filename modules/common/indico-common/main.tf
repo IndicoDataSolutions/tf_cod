8b3f18dc4dc7cb88658eb7680858ee869e588c7a
Lucas Telles <lucas.telles@indicodata.ai>
2025-01-09 13:27:01 -0300

    Add provider definition

--- modules/common/indico-common/main.tf
+++ modules/common/indico-common/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "integrations/github"
       version = "5.34.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
   }
 }
 
