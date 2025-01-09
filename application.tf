f25c6d757ee29404382d2d5940c068e990726936
Lucas Telles <lucas.telles@indicodata.ai>
2025-01-09 20:42:32 -0300

    Enable cluster autoscaler

--- application.tf
+++ application.tf
@@ -554,6 +554,7 @@ aws-load-balancer-controller:
     vpcId: ${local.network[0].indico_vpc_id}
     region: ${var.region}
 cluster-autoscaler:
+  enabled: true
   cluster-autoscaler:
     awsRegion: ${var.region}
     image:
