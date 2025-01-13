1722006218722b76df6c0c9644766d78910004ea
Lucas Telles <lucas.telles@indicodata.ai>
2025-01-13 14:46:45 -0300

    Test moved block

--- moved.tf
+++ moved.tf
@@ -12,3 +12,8 @@ moved {
   from = module.cluster.aws_iam_role.node_role
   to   = module.iam.module.create_eks_node_role[0].aws_iam_role.role
 }
+
+moved {
+  from = github_repository_file.argocd-application-yaml[0]
+  to   = module.intake[0].module.intake_application.github_repository_file.argocd-application-yaml[0]
+}
