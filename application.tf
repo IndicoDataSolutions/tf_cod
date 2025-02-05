24bed149ba4539f6f6a674da45af17a855e7c0e9
Andrey Chmykh <andrey.chmykh@indico.io>
2025-02-04 22:36:18 -0500

    Add region to external-dns

--- application.tf
+++ application.tf
@@ -137,6 +137,9 @@ external-dns:
     - ${local.dns_zone_name}
 
   provider: aws
+  env:
+    - name: AWS_DEFAULT_REGION
+      value: ${var.region}
   policy: sync
   sources:
     - service
@@ -163,6 +166,9 @@ external-dns:
     - ${local.dns_zone_name}
 
   provider: aws
+  env:
+    - name: AWS_DEFAULT_REGION
+      value: ${var.region}
   policy: sync
   sources:
     - service
@@ -181,7 +187,9 @@ alternate-external-dns:
     - "--aws-assume-role=${var.aws_primary_dns_role_arn}"
 
   provider: aws
-
+  env:
+    - name: AWS_DEFAULT_REGION
+      value: ${var.region}
   policy: sync
   sources:
     - ingress
