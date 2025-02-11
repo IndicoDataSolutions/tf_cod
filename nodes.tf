1fa5639f61872dc432fa018c6d65bd48d0f497a7
Andrey Chmykh <andrey.chmykh@indicodata.ai>
2025-02-11 12:52:35 -0500

    tomap(null)?

--- nodes.tf
+++ nodes.tf
@@ -139,7 +139,7 @@ locals {
     }
   }
 
-  default_node_groups = merge((var.insights_enabled ? local.insights_default_node_groups : tomap({})), (var.ipa_enabled ? local.intake_default_node_groups : tomap({})))
+  default_node_groups = merge((var.insights_enabled ? local.insights_default_node_groups : tomap(null)), (var.ipa_enabled ? local.intake_default_node_groups : tomap(null)))
 
   node_groups = var.node_groups == null ? local.default_node_groups : var.node_groups 
 }
