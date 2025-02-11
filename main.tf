fd1c8cb32a3886a599a398e8f3dd9490692fcb53
Andrey Chmykh <andrey.chmykh@indicodata.ai>
2025-02-10 23:05:12 -0500

    Fix the s3 module override default value

--- main.tf
+++ main.tf
@@ -206,7 +206,7 @@ module "security-group" {
 
 module "s3-storage" {
   source                             = "app.terraform.io/indico/indico-aws-buckets/mod"
-  version                            = "4.2.1"
+  version                            = "4.2.2"
   force_destroy                      = true # allows terraform to destroy non-empty buckets.
   label                              = var.label
   kms_key_arn                        = module.kms_key.key.arn
