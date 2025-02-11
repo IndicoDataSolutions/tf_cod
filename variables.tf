8575c5f0cf7208ce8568b8b65cf307dc2dc74485
Andrey Chmykh <andrey.chmykh@indicodata.ai>
2025-02-11 10:29:07 -0500

    Updated node group logic so we don't need to overwrite as often

--- variables.tf
+++ variables.tf
@@ -197,6 +197,8 @@ variable "k8s_version" {
 }
 
 variable "node_groups" {
+  default = null
+  description = "Override for the node groups assigned to the cluster. If not supplied, the node groups will be determined from intake and insights defaults"
 }
 
 variable "node_bootstrap_arguments" {
