c4097bb8a3ce0f400003faf8340037e250bba0d3
Lucas Telles <lucas.telles@indicodata.ai>
2025-02-12 15:58:16 -0300

    Test iam for karpenter

---
+++ modules/common/karpenter/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "node_role_arn" {
+  type = string
+}
+
+variable "node_role_name" {
+  type = string
+}
+
