38eef315c71f321e3875ebf6d1398b08a9af0a32
Lucas Telles <lucas.telles@indicodata.ai>
2025-03-21 19:10:47 -0300

    Change consolidationPolicy

--- modules/common/karpenter/main.tf
+++ modules/common/karpenter/main.tf
@@ -137,7 +137,7 @@ ${yamlencode([for k, v in local.karpenter_node_pools : {
     expireAfter            = "Never"
     terminationGracePeriod = "24h"
     disruption = {
-      consolidationPolicy = "WhenEmptyOrUnderutilized"
+      consolidationPolicy = "WhenEmpty"
       consolidateAfter    = "1m"
     }
     limits = {
