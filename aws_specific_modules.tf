1d3e1ac8ab490134c75fb5bec78947e903350d3c
Lucas Telles <lucas.telles@indicodata.ai>
2025-04-08 16:01:26 -0300

    [DEV-13372] Update guardduty addon to match eks 1.32 (#421)

--- aws_specific_modules.tf
+++ aws_specific_modules.tf
@@ -114,7 +114,7 @@ resource "aws_eks_addon" "guardduty" {
 
   cluster_name      = var.label
   addon_name        = "aws-guardduty-agent"
-  addon_version     = "v1.7.1-eksbuild.1"
+  addon_version     = "v1.9.0-eksbuild.2"
   resolve_conflicts = "OVERWRITE"
 
   preserve = true
