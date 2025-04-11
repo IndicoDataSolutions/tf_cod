c6f509634f880ef0c4571d61bc110f27d0f05fea
Lucas Telles <lucas.telles@indicodata.ai>
2025-04-11 11:57:49 -0300

    Update smoketests chart repo variable

--- variables.tf
+++ variables.tf
@@ -399,7 +399,7 @@ variable "ipa_smoketest_values" {
 
 variable "ipa_smoketest_repo" {
   type    = string
-  default = "https://harbor.devops.indico.io/chartrepo/indico-charts"
+  default = "oci://harbor.devops.indico.io/indico-charts"
 }
 
 variable "ipa_smoketest_version" {
