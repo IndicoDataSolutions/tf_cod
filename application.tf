24dcd17efa9e28fab428320e4fa282769893ffb3
Lucas Telles <lucas.telles@indicodata.ai>
2025-04-21 11:57:59 -0300

    Move migrations to indico-pre-reqs

--- application.tf
+++ application.tf
@@ -347,13 +347,6 @@ module "karpenter" {
 # Once (if) the secrets operator is set up, we can deploy the common charts
 locals {
   indico_crds_values = [<<EOF
-migrations:
-  image:
-    registry: ${var.image_registry}
-  vaultSecretsOperator:
-    updateCRDs: ${var.secrets_operator_enabled}
-  opentelemetryOperator:
-    updateCRDs: ${var.monitoring_enabled}
 aws-ebs-csi-driver:
   image:
     repository: ${var.image_registry}/public.ecr.aws/ebs-csi-driver/aws-ebs-csi-driver
@@ -575,6 +568,15 @@ global:
   host: ${local.dns_name}
   image:
     registry: ${var.image_registry}
+crunchy-pgo:
+  enabled: ${var.ipa_enabled || var.insights_enabled}
+migrations:
+  image:
+    registry: ${var.image_registry}
+  vaultSecretsOperator:
+    updateCRDs: ${var.secrets_operator_enabled}
+  opentelemetryOperator:
+    updateCRDs: ${var.monitoring_enabled}
 storage:
   ebsStorageClass:
     enabled: true
