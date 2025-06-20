7dca276d33c079380c39137aeb248e9e557790cd
Nathan Okolita <Nathan.Okolita@gmail.com>
2025-06-20 12:23:01 -0400

    [DEV-13765] Fix helm provider (#444)

--- azure/main.tf
+++ azure/main.tf
@@ -22,7 +22,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.15.0"
+      version = "~> 2.15.0"
     }
     argocd = {
       source  = "oboukili/argocd"
