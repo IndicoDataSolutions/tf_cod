a81251cd7ec0cb74a11ca31dc462160b47fdd7a7
Lucas Telles <lucas.telles@indicodata.ai>
2025-06-26 15:15:19 -0300

    Add upgrade_install to monitoring chart

--- modules/common/indico-common/main.tf
+++ modules/common/indico-common/main.tf
@@ -131,7 +131,7 @@ resource "helm_release" "monitoring" {
   version          = var.monitoring_version
   wait             = false
   timeout          = "1800" # 30 minutes
-  force_update     = true
+  upgrade_install     = true
 
   values = var.monitoring_values
 }
