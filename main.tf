981542192a52a1bde897d550c68340d6d77e9134
Lucas Telles <lucas.telles@indicodata.ai>
2025-07-31 14:41:02 -0300

    [DEV-14080] Fix kubernetes provider version

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.37"
+      version = "2.38.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
