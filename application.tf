63e92789df3afa642e9b3bd520a86dcd769bca61
Lucas Telles <lucas.telles@indicodata.ai>
2025-10-20 12:07:42 -0300

    Fix nlb annotations indentetion

--- application.tf
+++ application.tf
@@ -978,12 +978,12 @@ annotations: {}
       ${indent(6, local.loadbalancer_annotation_config)}
       external:
         enabled: ${var.network_allow_public}
-        annotations:
-          service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
-          service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: '60'
-          service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: 'true'
-          service.beta.kubernetes.io/aws-load-balancer-type: nlb
-          service.beta.kubernetes.io/aws-load-balancer-ssl-ports: "https"
+      annotations:
+        service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
+        service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: '60'
+        service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: 'true'
+        service.beta.kubernetes.io/aws-load-balancer-type: nlb
+        service.beta.kubernetes.io/aws-load-balancer-ssl-ports: "https"
       internal:
         enabled: ${local.internal_elb}
         annotations:
