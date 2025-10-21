3c67d95f8635320373e3368dc896adc7054f0625
Lucas Telles <lucas.telles@indicodata.ai>
2025-10-20 22:12:47 -0300

    Fix alb annotations indentetion

--- application.tf
+++ application.tf
@@ -10,15 +10,15 @@ locals {
   acm_arn               = var.use_acm && var.acm_arn == "" ? aws_acm_certificate_validation.acm[0].certificate_arn : var.acm_arn
   waf_arn               = var.enable_waf == true && var.waf_arn == "" ? aws_wafv2_web_acl.wafv2-acl[0].arn : var.waf_arn
   alb_annotations       = <<EOF
-    alb.ingress.kubernetes.io/target-type: ip
-    alb.ingress.kubernetes.io/scheme: ${var.network_allow_public == true ? "internet-facing" : "internal"}
-    alb.ingress.kubernetes.io/subnets: ${var.network_allow_public ? join(", ", local.environment_public_subnet_ids) : join(", ", local.environment_private_subnet_ids)}
-    ${local.waf_arn != "" ? "alb.ingress.kubernetes.io/wafv2-acl-arn: ${local.waf_arn}" : ""}
-    ${local.acm_arn != "" ? "alb.ingress.kubernetes.io/certificate-arn: ${local.acm_arn}" : ""}
-    ${var.use_static_ssl_certificates == false ? "cert-manager.io/cluster-issuer: zerossl" : ""}
-    alb.ingress.kubernetes.io/ssl-redirect: "443"
-    alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
-    alb.ingress.kubernetes.io/ssl-policy: "ELBSecurityPolicy-TLS-1-2-2017-01"
+  alb.ingress.kubernetes.io/target-type: ip
+  alb.ingress.kubernetes.io/scheme: ${var.network_allow_public == true ? "internet-facing" : "internal"}
+  alb.ingress.kubernetes.io/subnets: ${var.network_allow_public ? join(", ", local.environment_public_subnet_ids) : join(", ", local.environment_private_subnet_ids)}
+  ${local.waf_arn != "" ? "alb.ingress.kubernetes.io/wafv2-acl-arn: ${local.waf_arn}" : ""}
+  ${local.acm_arn != "" ? "alb.ingress.kubernetes.io/certificate-arn: ${local.acm_arn}" : ""}
+  ${var.use_static_ssl_certificates == false ? "cert-manager.io/cluster-issuer: zerossl" : ""}
+  alb.ingress.kubernetes.io/ssl-redirect: "443"
+  alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
+  alb.ingress.kubernetes.io/ssl-policy: "ELBSecurityPolicy-TLS-1-2-2017-01"
   EOF
   efs_values = var.include_efs == true ? [<<EOF
   storage:
@@ -91,7 +91,7 @@ app-edge:
     ingressClassName: alb
     annotations:
       nginx.ingress.kubernetes.io/service-upstream: ${var.enable_service_mesh ? "true" : "false"}
-    ${local.alb_annotations}
+${indent(6, local.alb_annotations)}
   EOT
     ) : (<<EOT
 app-edge:
