a634bd765813682c0adee052c49e94740dd7be3d
Lucas Telles <lucas.telles@indicodata.ai>
2026-01-05 16:58:12 -0300

    Add default values for vault-operator module vars (#523)

--- modules/common/vault-secrets-operator-setup/variables.tf
+++ modules/common/vault-secrets-operator-setup/variables.tf
@@ -13,9 +13,17 @@ variable "lambda_sns_forwarder_enabled" {
   type    = bool
   default = false
 }
-variable "lambda_sns_forwarder_iam_principal_arn" {}
+variable "lambda_sns_forwarder_iam_principal_arn" {
+  type        = string
+  default     = ""
+  description = "IAM principal ARN for the lambda SNS forwarder"
+}
 
-variable "account_id" {}
+variable "account_id" {
+  type        = string
+  default     = ""
+  description = "AWS account ID"
+}
 
 locals {
   account_region_name = lower("${var.account}-${var.region}-${var.name}")
