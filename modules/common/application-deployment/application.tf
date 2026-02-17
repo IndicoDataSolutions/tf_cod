83196b80034882b84bfe4149552b0f90c4b068cc
NathanOkolita <Nathan.Okolita@indicodata.ai>
2026-02-17 08:08:34 -0500

    [DEV-14834] Update logic for finding exising github file values. Added debugging to see where we are failing. try 17

--- modules/common/application-deployment/application.tf
+++ modules/common/application-deployment/application.tf
@@ -40,7 +40,7 @@ locals {
     ""
   )
   
-  helm_values_to_use            = local.existing_exists && local.helm_values_from_file != "" ? local.helm_values_from_file : var.helm_values
+  helm_values_to_use            = local.existing_exists && local.helm_values_from_file != "" ? indent(0, local.helm_values_from_file) : var.helm_values
 
   # --- Debug: inspect what was fetched/parsed (use outputs below to view) ---
   debug_fetch_exists                 = data.external.fetch_argo_application.result.exists
@@ -129,9 +129,9 @@ spec:
         
         - name: HELM_TF_COD_VALUES
           value: |
-            ${indent(12, var.terraform_helm_values)}
+              ${var.terraform_helm_values}
         - name: HELM_VALUES
           value: |
-            ${indent(12, local.helm_values_to_use)}
+              ${local.helm_values_to_use}
 ARGO_APPLICATION_YAML_END
 }
