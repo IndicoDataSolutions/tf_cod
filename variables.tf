83909e1a23eb8ef920c5257cbae9a0cc0b9c7bf9
Lucas Telles <lucas.telles@indicodata.ai>
2026-03-05 16:01:15 -0300

    [DEV-14958] Upgrade s3 storage version and add needed variables (#539)

--- variables.tf
+++ variables.tf
@@ -177,18 +177,6 @@ variable "bucket_versioning" {
   description = "Enable bucket object versioning"
 }
 
-variable "submission_expiry" {
-  type        = number
-  description = "The number of days to retain submissions"
-  default     = 30
-}
-
-variable "uploads_expiry" {
-  type        = number
-  description = "The number of days to retain uploads"
-  default     = 30
-}
-
 ### cluster
 variable "name" {
   type        = string
@@ -1844,4 +1832,16 @@ variable "disable_nginx_ingress" {
   type        = bool
   default     = false
   description = "Toggle for disabling Nginx Ingress"
+}
+
+variable "s3_cleanup_noncurrent_days" {
+  type        = number
+  default     = 7
+  description = "The number of days to retain non-current versions of S3 objects"
+}
+
+variable "s3_retain_backup_days" {
+  type        = number
+  default     = 7
+  description = "The number of days to retain S3 backups"
 }
