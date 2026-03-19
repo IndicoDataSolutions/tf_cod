8d52226b0ef0f53b87f732b622b45134be6fd920
Lucas Telles <lucas.telles@indicodata.ai>
2026-03-19 11:29:12 -0300

    Pin cloudposse/utils provider version (#550)

--- modules/harness/versions.tf
+++ modules/harness/versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     helm = {
-      source  = "hashicorp/helm"
+      source = "hashicorp/helm"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = ">= 0.14.0"
+      version = "2.3.0"
     }
   }
 }
