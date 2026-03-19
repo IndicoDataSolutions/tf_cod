3730e78a670733fba5f754bff4c2c955d7660101
Lucas Telles <lucas.telles@indicodata.ai>
2026-03-19 11:27:57 -0300

    Update version

--- modules/harness/versions.tf
+++ modules/harness/versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     helm = {
-      source  = "hashicorp/helm"
+      source = "hashicorp/helm"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = "2.1.0"
+      version = "2.3.0"
     }
   }
 }
