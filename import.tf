252e8c4919cd10abae928fa794af3af25ca686fd
NathanOkolita <Nathan.Okolita@indicodata.ai>
2026-03-24 11:42:23 -0400

    [DEV-14792] Allow optionally importing indico_core helm release. This is important for upgrading clusters that have existing postgres deployments. Try 2

--- import.tf
+++ import.tf
@@ -3,8 +3,14 @@ variable "import_indico_core" {
   description = "One-shot: import existing Helm release indico/indico-core into state"
   default     = false
 }
+
+# import blocks support for_each, not count.
+locals {
+  import_indico_core_ids = var.import_indico_core && !var.multitenant_enabled ? { core = "indico/indico-core" } : {}
+}
+
 import {
-  count = var.import_indico_core ? 1 : 0
-  to = module.indico-common[0].helm_release.indico_core
-  id = "indico/indico-core"
+  for_each = local.import_indico_core_ids
+  to       = module.indico-common[0].helm_release.indico_core
+  id       = each.value
 }
