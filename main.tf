a0561ca4c90b912bdb94c9bbdce8f0657184f3e2
ashmuck <andrey.chmykh@indicodata.ai>
2026-03-25 14:19:02 -0400

    fix moved block

--- main.tf
+++ main.tf
@@ -197,7 +197,7 @@ module "security-group" {
 module "s3-storage" {
   count                              = var.load_environment == "" ? 1 : 0
   source                             = "app.terraform.io/indico/indico-aws-buckets/mod"
-  version                            = "4.6.2"
+  version                            = "4.6.3"
   force_destroy                      = true # allows terraform to destroy non-empty buckets.
   label                              = var.label
   kms_key_arn                        = local.environment_kms_key_arn
